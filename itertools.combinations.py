3be427956d8f00408e59aeb94f21e41713639830
Ross Reisman <Ross.Reisman@gmail.com>
2022-07-04 13:14:46 -0400

    Create Combinations

---
+++ itertools.combinations.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+
+string = input().split()
+combo = combinations(string[0], string[1])
+print(combo)
+
+from itertools import combinations
+
+s = input().split()
+string, number = sorted(s[0]), int(s[1])
+for i in range(1, number + 1):
+    print(*list(map("".join, combinations(string, i))), sep="\n")
