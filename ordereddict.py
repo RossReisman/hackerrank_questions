0b55936c449dd38e8a65d3174a27db3145c18469
Ross Reisman <Ross.Reisman@gmail.com>
2022-07-04 10:46:45 -0400

    Update ordereddict.py

--- ordereddict.py
+++ ordereddict.py
@@ -9,5 +9,16 @@ item_price
 item_dict = OrderedDict()
 
 for item in range(N):
-
+    item_dict.
     print(item_dict)
+
+
+from collections import OrderedDict
+
+n, item_list = int(input()), OrderedDict()
+
+for _ in range(n):
+    item, price = input().rsplit(' ',1)
+    item_list.setdefault(item, 0)
+    item_list[item] += int(price)
+[print(i, v) for i, v in item_list.items()]
