1747e7abed48788c2655cf9ce047489098d42ce9
Ross Reisman <Ross.Reisman@gmail.com>
2022-07-07 05:02:51 -0400

    Solutions for Polar Coordinates

--- polar_coordinates.py
+++ polar_coordinates.py
@@ -4,4 +4,15 @@ comp_num = input().split()
 z, x, y = comp_num[0], comp_num[1], comp_num[2]
 
 
-print("{:.3f}".format)
+print("{:.3f}".format(x, y))
+
+import cmath
+
+r = complex(input().strip())
+
+print(cmath.polar(r)[0])
+print(cmath.polar(r)[1])
+
+import cmath
+
+[print(round(i, 3)) for i in cmath.polar(complex(input()))]
