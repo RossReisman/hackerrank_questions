dd235057ec77f3ee7712a1267d921a3d17a78b5e
Ross Reisman <Ross.Reisman@gmail.com>
2022-07-08 05:10:42 -0400

    Create exceptions

---
+++ exceptions.py
@@ -0,0 +1,11 @@
+err_codes = {v:'ValueError', e:'ZeroDivisionError'}
+
+num_cases = map(int, input())
+values = input().split()
+
+for i in values try:
+    a/b
+if b = 0 except:
+    print("Error Code:", e)
+elif b != int except:
+    print("Error Code:", v)
