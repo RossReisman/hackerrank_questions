912df07c1c23211a4b23d5f790d1305ae234fb87
Ross Reisman <Ross.Reisman@gmail.com>
2022-07-20 05:29:30 -0400

    Create Set Union

---
+++ set_union.py
@@ -0,0 +1,9 @@
+num_eng_stu = int(input())
+eng_stu_roll = input().split()
+num_fre_stu = int(input())
+fre_stu_roll = input().split()
+
+eng_stu_set = set(eng_stu_roll)
+fre_stu_set = set(fre_stu_roll)
+
+print(eng_stu_set.union(fre_stu_set))
