c34d9bc75e3f7b7f361a021e0fe1f0117b0ed2b8
Ross Reisman <Ross.Reisman@gmail.com>
2022-08-27 07:15:07 -0400

    Create Floor Ceil Rint

---
+++ floor_ceil_rint.py
@@ -0,0 +1,21 @@
+# First Attempt
+
+import numpy as np
+
+np.set_printoptions(legacy="1.13")
+
+A = list(map(float, input().split()))
+a = np.array(A)
+print(np.floor(A), np.ceil(A), np.rint(A), sep="\n")
+
+# Solution
+
+import numpy as np
+
+np.set_printoptions(legacy="1.13")
+
+A = list(map(float, input().split()))
+a = np.array(A)
+print(np.floor(A), np.ceil(A), np.rint(A), sep="\n")
+
+# Got it right!
